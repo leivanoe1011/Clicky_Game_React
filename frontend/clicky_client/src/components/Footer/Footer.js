36c23988ff3538761f1e54755bb92cd880dfde08
Noe Leiva <leiva.noe.86@gmail.com>
2020-09-26 14:54:25 -0500

    created footer component

---
+++ frontend/clicky_client/src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="bg-info footer mt-5 py-3">
+                <div className="container">
+                    <span className="text-white">
+                        ClickyGame made by: James Hooven
+                    </span>
+                </div>
+            </footer>
+        )
+    }
+}
+
+export default Footer
