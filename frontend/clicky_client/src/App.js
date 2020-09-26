e95b6f570ee83e429f0ffc604f1fe073d63f0e30
Noe Leiva <leiva.noe.86@gmail.com>
2020-09-26 14:51:45 -0500

    created component folder

---
+++ frontend/clicky_client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Pictures from "./components/Pictures/Pictures"
+import Nav from "./components/Navbar/Navbar"
+import Footer from "./components/Footer/Footer"
+
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Pictures />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+
+
